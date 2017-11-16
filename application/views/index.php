7678664293025517703ddbb251e68633ccf91364
thisismyshadow <thisismyshadow@gmail.com>
2017-11-16 18:08:27 +0400

    added ci application folder

---
+++ application/views/index.php
@@ -0,0 +1,6 @@
+<h3>Shelf has <?php echo $shelf->count()?> books</h3>
+<form action="<?=base_url("ShelfController/put")?>" method="POST">
+	<input name='book' value="<?php echo isset($book)?$book:''?>"/>
+	<input type="submit"/>
+</form>
+<a href="<?php echo base_url("ShelfController/get_and_read")?>">get</a>
